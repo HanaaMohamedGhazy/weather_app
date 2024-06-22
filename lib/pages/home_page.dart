5bebb0c7246e1c47545ae47ad6d112b8ba3ff95c
Hanaa Mohamed Ahmed Ghazy <91637211+Hanaa-Mohamed-Ghazy@users.noreply.github.com>
2024-06-22 08:30:25 +0300

    first commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:weather_app/models/weather_model.dart';
+import 'package:weather_app/pages/search_page.dart';
+import 'package:weather_app/providers/weather_provider.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  void updateUi() {
+    setState(() {});
+  }
+
+  WeatherModel? weatherData;
+  @override
+  Widget build(BuildContext context) {
+    weatherData = Provider.of<WeatherProvider>(context).weatherData;
+
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return SearchPage(
+                  updateUi: updateUi,
+                );
+              }));
+            },
+            icon: const Icon(Icons.search),
+          ),
+        ],
+        title: const Text('Weather App',style: TextStyle(
+          color:Colors.white
+        ),),
+      ),
+      body: Provider.of<WeatherProvider>(context).weatherData == null
+          ?Container(
+        decoration: const BoxDecoration(
+          image:  DecorationImage(
+            image: AssetImage('assets/images/images (2).jpg'),
+            fit: BoxFit.cover,
+
+          ),),
+          child:Center(child:
+            Column(
+                mainAxisSize: MainAxisSize.max,
+                children: const [
+                  Text(
+                    'there is no weather 😔 ',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40,
+                    ),
+                  ),
+                  Text(
+                    'start searching',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40,
+                    ),
+                  )
+                ],
+              ),
+            ))
+          : Container(
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                colors: [
+                  weatherData!.getThemeColor(),
+                  weatherData!.getThemeColor()[300]!,
+                  weatherData!.getThemeColor()[100]!,
+                ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              )),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Spacer(
+                    flex: 3,
+                  ),
+                  Text(
+                    Provider.of<WeatherProvider>(context).cityName!,
+                    style: const TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    'updated at : ${weatherData!.date.hour.toString()}:${weatherData!.date.minute.toString()}',
+                    style: const TextStyle(
+                      fontSize: 22,
+                    ),
+                  ),
+                  const Spacer(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Image.asset(weatherData!.getImage()),
+                      Text(
+                        weatherData!.temp.toInt().toString(),
+                        style: const TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Column(
+                        children: [
+                          Text('maxTemp :${weatherData!.maxTemp.toInt()}'),
+                          Text('minTemp : ${weatherData!.minTemp.toInt()}'),
+                        ],
+                      ),
+                    ],
+                  ),
+                  const Spacer(),
+                  Text(
+                    weatherData!.weatherStateName,
+                    style: const TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const Spacer(
+                    flex: 5,
+                  ),
+                ],
+              ),
+            ),
+    );
+  }
+}
